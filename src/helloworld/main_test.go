6226b22d8d0d79b313a913217287c16ef10f1ca8
adamazing <adamazing@gmail.com>
2020-11-20 06:17:09 +1300

    Add Goodbye World
    Signed-off-by: adamazing <adamazing@gmail.com>

--- src/helloworld/main_test.go
+++ src/helloworld/main_test.go
@@ -5,6 +5,13 @@ import "testing"
 func TestHelloWorld(t *testing.T) {
 	var msg = helloWorld()
 	if msg != "Hello World!" {
-		t.Errorf("helloWorld did not work, expected Hello World! but got %s", msg)
+		t.Errorf("helloWorld did not work, expected 'Hello World!' but got %s", msg)
+	}
+}
+
+func TestGoodbyeWorld(t *testing.T) {
+	var msg = goodbyeWorld()
+	if msg != "Goodbye Cruel World!" {
+		t.Errorf("goodbyeWorld did not work, expected 'Goodbye Cruel World!' but got %s", msg)
 	}
 }
